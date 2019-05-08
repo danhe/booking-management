9ace1cb7d6d30ca3c20a25408aba8d1b29d6788c
Dan HE <dan_emilie@yahoo.com>
2019-05-08 18:53:46 +0200

    generate mission after creating booking and reservation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,11 @@
 class Booking < ApplicationRecord
+  after_create :generate_missions
+
   belongs_to :listing
+
+  private
+
+  def generate_missions
+    GenerateMissionsService.new(self).call
+  end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,11 @@
 class Reservation < ApplicationRecord
+  after_create :generate_missions
+
   belongs_to :listing
+
+  private
+
+  def generate_missions
+    GenerateMissionsService.new(nil, self).call
+  end
 end
